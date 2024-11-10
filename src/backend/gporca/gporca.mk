9d14871b6c1b2a49d05d37a08e5057fa6ff16405
kotb <zavgmaxden@mail.ru>
2024-11-10 17:30:21 +0300

    changes for local work, dont push them

--- src/backend/gporca/gporca.mk
+++ src/backend/gporca/gporca.mk
@@ -4,4 +4,5 @@ override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libnaucrates/include $
 override CPPFLAGS := -I$(top_builddir)/src/backend/gporca/libgpdbcost/include $(CPPFLAGS)
 # Do not omit frame pointer. Even with RELEASE builds, it is used for
 # backtracing.
-override CXXFLAGS := -Werror -Wextra -Wpedantic -Wno-variadic-macros -fno-omit-frame-pointer $(CXXFLAGS)
+override CPPFLAGS := -Wextra -Wpedantic -Wno-variadic-macros -Wunused-but-set-variable -fno-omit-frame-pointer $(CPPFLAGS)
+override CPPFLAGS := -std=c++11 $(CPPFLAGS)
